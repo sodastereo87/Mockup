2d750ff936c6b5a62ce93bbb8706114deac3aa6e
sodastereo <f_er_@hotmail.com>
2023-11-11 16:14:11 -0500

    adding dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,47 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import './App.css';
-import DropDown from './components/dropdown'
+import DropDown from './components/dropdown';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p style={{ fontWeight: 'bold',textDecoration: 'underline' }}>
-         Document Upload
-        </p>
-      </header>
-      <div>
-        <DropDown />
+    <Container>
+      <div className="App">
+        <header className="App-header">
+          <p style={{ fontWeight: 'bold', textDecoration: 'underline' }}>
+            Document Upload
+          </p>
+        </header>
+
+        <Row>
+          <Col>
+            <div>
+              <DropDown />
+            </div>
+          </Col>
+          <Col>
+            <div>
+              <DropDown />
+            </div>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <div>
+              <DropDown />
+            </div>
+          </Col>
+          <Col>
+            <div>
+              <DropDown />
+            </div>
+          </Col>
+        </Row>
       </div>
-    </div>
+    </Container>
   );
 }
 
