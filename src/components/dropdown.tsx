64297f2f9b47fce1a26dd2c49d1700519f739bf8
sodastereo <f_er_@hotmail.com>
2023-11-13 20:15:24 -0500

    adding some style

--- src/components/dropdown.tsx
+++ src/components/dropdown.tsx
@@ -1,14 +1,15 @@
 import Dropdown from 'react-bootstrap/Dropdown';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import dropCSS from './dropdown.module.css'
 
 function DropDown() {
   return (
-    <Dropdown>
-      <Dropdown.Toggle variant="" id="dropdown-basic">
+    <Dropdown className={dropCSS.menu}>
+      <Dropdown.Toggle variant="" id="dropdown-basic" >
       Select Import Name
       </Dropdown.Toggle>
 
-      <Dropdown.Menu>
+      <Dropdown.Menu className={dropCSS.menu2}>
         <Dropdown.Item href="#/action-1">Name 1</Dropdown.Item>
         <Dropdown.Item href="#/action-2">Name 2</Dropdown.Item>
         <Dropdown.Item href="#/action-3">Name 3</Dropdown.Item>
